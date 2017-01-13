9c14da2a12003e01c2c8f52e987bf3b2f13ed66d
Oliver Benns <oliverbenns@gmail.com>
2017-01-13 17:08:06 +0700

    Fix option params

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 import qs from 'qs';
 import config from './config';
 
-const constructUrl = (path, params, type) => {
+const constructUrl = (path, params) => {
   const queryParams = qs.stringify({
+    ...params,
     client_id: config.clientId,
-    client_secret: config.clientSecret
+    client_secret: config.clientSecret,
   });
 
   return config.host + '/ghost/api/v0.1' + path +  '?' + queryParams;
